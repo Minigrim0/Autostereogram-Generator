118e56d28dad85df8009723de4c181017706fe00
minigrim0 <grimauflorent@gmail.com>
2022-04-30 17:43:13 +0200

    [Upd] window functinos

--- src/ui_MainWindow.py
+++ src/ui_MainWindow.py
@@ -1,10 +1,19 @@
-from PyQt5.QtWidgets import (
-    QWidget, QVBoxLayout, QPushButton, QLabel, QSizePolicy, QHBoxLayout)
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import QRect, Qt, QCoreApplication, QMetaObject
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'autostereogram_generatorpgSbja.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 
-class UI_MainWindow(object):
+class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
@@ -20,8 +29,7 @@ class UI_MainWindow(object):
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(30)
-        sizePolicy.setHeightForWidth(
-            self.Title.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.Title.sizePolicy().hasHeightForWidth())
         self.Title.setSizePolicy(sizePolicy)
         font = QFont()
         font.setPointSize(20)
@@ -41,17 +49,28 @@ class UI_MainWindow(object):
         self.verticalLayoutOriginal.setObjectName(u"verticalLayoutOriginal")
         self.ImagePreviewLabel = QLabel(self.verticalLayoutWidget)
         self.ImagePreviewLabel.setObjectName(u"ImagePreviewLabel")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.ImagePreviewLabel.sizePolicy().hasHeightForWidth())
+        self.ImagePreviewLabel.setSizePolicy(sizePolicy1)
         font1 = QFont()
         font1.setPointSize(14)
         self.ImagePreviewLabel.setFont(font1)
-        self.ImagePreviewLabel.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        self.ImagePreviewLabel.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
 
         self.verticalLayoutOriginal.addWidget(self.ImagePreviewLabel)
+
+        self.MapPreview = QLabel(self.verticalLayoutWidget)
+        self.MapPreview.setObjectName(u"MapPreview")
+
+        self.verticalLayoutOriginal.addWidget(self.MapPreview)
 
         self.LoadImage = QPushButton(self.verticalLayoutWidget)
         self.LoadImage.setObjectName(u"LoadImage")
 
         self.verticalLayoutOriginal.addWidget(self.LoadImage)
+
 
         self.horizontalLayout.addLayout(self.verticalLayoutOriginal)
 
@@ -59,35 +78,43 @@ class UI_MainWindow(object):
         self.verticalLayoutResult.setObjectName(u"verticalLayoutResult")
         self.ImageResultLabel = QLabel(self.verticalLayoutWidget)
         self.ImageResultLabel.setObjectName(u"ImageResultLabel")
+        sizePolicy1.setHeightForWidth(self.ImageResultLabel.sizePolicy().hasHeightForWidth())
+        self.ImageResultLabel.setSizePolicy(sizePolicy1)
         self.ImageResultLabel.setFont(font1)
-        self.ImageResultLabel.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        self.ImageResultLabel.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
 
         self.verticalLayoutResult.addWidget(self.ImageResultLabel)
+
+        self.ResultPreview = QLabel(self.verticalLayoutWidget)
+        self.ResultPreview.setObjectName(u"ResultPreview")
+
+        self.verticalLayoutResult.addWidget(self.ResultPreview)
 
         self.SaveImage = QPushButton(self.verticalLayoutWidget)
         self.SaveImage.setObjectName(u"SaveImage")
 
         self.verticalLayoutResult.addWidget(self.SaveImage)
 
+
         self.horizontalLayout.addLayout(self.verticalLayoutResult)
 
+
         self.MainBox.addLayout(self.horizontalLayout)
+
 
         self.retranslateUi(Dialog)
 
         QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(
-            QCoreApplication.translate("Dialog", u"Dialog", None))
-        self.Title.setText(
-            QCoreApplication.translate(
-                "Dialog", u"Autostereogram Generator", None))
-        self.ImagePreviewLabel.setText(
-            QCoreApplication.translate("Dialog", u"Map Preview", None))
-        self.LoadImage.setText(
-            QCoreApplication.translate("Dialog", u"Select an image", None))
-        self.ImageResultLabel.setText(
-            QCoreApplication.translate("Dialog", u"Result", None))
-        self.SaveImage.setText(
-            QCoreApplication.translate("Dialog", u"Save Image", None))
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.Title.setText(QCoreApplication.translate("Dialog", u"Autostereogram Generator", None))
+        self.ImagePreviewLabel.setText(QCoreApplication.translate("Dialog", u"Map Preview", None))
+        self.MapPreview.setText("")
+        self.LoadImage.setText(QCoreApplication.translate("Dialog", u"Select an image", None))
+        self.ImageResultLabel.setText(QCoreApplication.translate("Dialog", u"Result", None))
+        self.ResultPreview.setText("")
+        self.SaveImage.setText(QCoreApplication.translate("Dialog", u"Save Image", None))
+    # retranslateUi
+
--- src/window.py
+++ src/window.py
@@ -4,11 +4,11 @@ from PyQt5.QtWidgets import QMainWindow
 
 import cv2
 
-from src.ui_MainWindow import UI_MainWindow
+from src.ui_MainWindow import Ui_Dialog
 from src.autostereogram import create_autoStereogram
 
 
-class Window(QMainWindow, UI_MainWindow):
+class Window(QMainWindow, Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -25,22 +25,16 @@ class Window(QMainWindow, UI_MainWindow):
         fname = QFileDialog.getOpenFileName(self)
         imagePath = fname[0]
         pixmap = QPixmap(imagePath)
-        self.ImagePreviewLabel.setPixmap(QPixmap(pixmap))
-        self.resize(10, 10)
+        self.MapPreview.setPixmap(QPixmap(pixmap))
 
         self.result = create_autoStereogram(fname[0])
-
-        if len(self.result.shape) < 3:
-            frame = cv2.cvtColor(self.result, cv2.COLOR_GRAY2RGB)
-        else:
-            frame = cv2.cvtColor(self.result, cv2.COLOR_BGR2RGB)
 
         h, w = self.result.shape[:2]
         bytesPerLine = 3 * w
         qimage = QImage(
-            frame.data, w, h, bytesPerLine, QImage.Format.Format_RGB888)
+            self.result.data, w, h, bytesPerLine, QImage.Format.Format_RGB888)
 
-        self.ImageResultLabel.setPixmap(QPixmap(qimage))
+        self.ResultPreview.setPixmap(QPixmap(qimage))
 
     def save_result(self):
         fname = QFileDialog.getOpenFileName(self)
